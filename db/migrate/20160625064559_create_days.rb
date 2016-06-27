f23f8c48a0e4a96a8a7ffb4ce89d135a744adb33
fi1446 <kamedashigeru@gmail.com>
2016-06-27 22:28:52 +0900

    delete days
    rails d scaffold days

--- db/migrate/20160625064559_create_days.rb
+++
@@ -1,9 +0,0 @@
-class CreateDays < ActiveRecord::Migration
-  def change
-    create_table :days do |t|
-      t.references :log, index: true, foreign_key: true
-
-      t.timestamps null: false
-    end
-  end
-end
